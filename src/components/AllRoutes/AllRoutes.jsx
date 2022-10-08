e23e5de4dfd56fa68ad9bb3466ef4a60a579754e
mugilmalathi <mugilarasan.malathi@gmail.com>
2022-10-08 12:15:32 +0530

    footer done

--- src/components/AllRoutes/AllRoutes.jsx
+++ src/components/AllRoutes/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
+import Footer from '../Footer/Footer'
 import Home from '../Home/Home'
 
 const AllRoutes = () => {
@@ -8,6 +9,7 @@ const AllRoutes = () => {
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "./footer.scss"
+
+import up from "../Assets/Svg/thumbs_up.svg"
+import down from "../Assets/Svg/thumbs_down.svg"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div>Is this offer useful?</div>
+      <div><img src={down} /></div>
+      <div><img src={up} /></div>
+      <div></div>
+    </div>
   )
 }
 
