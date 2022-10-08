f6091487fbd710b97c76d97e7eeca387a1f5f19c
mugilmalathi <mugilarasan.malathi@gmail.com>
2022-10-08 12:48:11 +0530

    final out

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./footer.scss"
 
 import up from "../Assets/Svg/thumbs_up.svg"
 import down from "../Assets/Svg/thumbs_down.svg"
 
 const Footer = () => {
+
+  // const[like, setLike] = useState(null)
+
+  // const handleLike = ()=>{
+  //   setLike(!like)
+  // }
+
   return (
     <div className='footer'>
       <div>Is this offer useful?</div>
